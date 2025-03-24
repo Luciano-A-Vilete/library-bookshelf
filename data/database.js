042dde287448e985ee5e2eecf84d020373c3e24c
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-03-24 20:00:34 -0300

    fixing db connection

--- data/database.js
+++ data/database.js
@@ -1,33 +1,35 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
 
-const mongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 let database;
 
-const initDb = (callback) => {
+const initDb = async (callback) => {
     if (database) {
         console.log('Database is already initialized!');
         return callback(null, database);
     }
-    mongoClient.connect(process.env.MONGODB_URI)
-    .then((client) =>{
-        database = client;
+
+    try {
+        const client = await MongoClient.connect(process.env.MONGODB_URI);
+        database = client.db(); // Aqui pegamos a referência ao banco de dados corretamente
+        console.log("Banco de dados conectado!");
         callback(null, database);
-    })
-    .catch((err) =>{
+    } catch (err) {
+        console.error("Erro ao conectar ao banco:", err);
         callback(err);
-    });
+    }
 };
 
 const getDatabase = () => {
     if (!database) {
-        throw Error('Database not initialized');
+        throw new Error('Database not initialized');
     }
     return database;
-}
+};
 
 module.exports = {
     initDb,
     getDatabase
-}
+};
